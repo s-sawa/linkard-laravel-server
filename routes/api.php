5e106d5279f9215879d876092a693c0078ad30b6
s-sawa <s.s.hrmeee21@gmail.com>
2023-09-09 22:36:00 +0900

    ProfileController変更

--- routes/api.php
+++ routes/api.php
@@ -5,12 +5,16 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\ProfileController;
 
 Route::post('/login', LoginController::class)->name('login');
 Route::post('/register', [RegisteredUserController::class, 'store']);
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/profile/me', [ProfileController::class, 'show'])->name('profile.me');
+    
     Route::post('/logout', LogoutController::class)->name('logout');
+    
 
     Route::get('user', function (Request $request) {
         return $request->user();
