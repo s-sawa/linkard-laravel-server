8a736f2c122db6d08f3b7d11e9a2613f0f23f2b1
s-sawa <s.s.hrmeee21@gmail.com>
2023-09-13 23:57:57 +0900

    QR発行対応

--- routes/api.php
+++ routes/api.php
@@ -5,14 +5,23 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\OtherProfileController;
 use App\Http\Controllers\ProfileController;
+use App\Models\Other;
 
 Route::post('/login', LoginController::class)->name('login');
 Route::post('/register', [RegisteredUserController::class, 'store']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/profile/me', [ProfileController::class, 'show'])->name('profile.me');
+    // 自分のプロフィールを登録する
     Route::post('/profile/me', [ProfileController::class, 'store'])->name('profile.me');
+    // 自分の登録プロフィールを取得する
+    Route::get('/profile/me', [ProfileController::class, 'show'])->name('profile.me');
+    // 自分の登録プロフィールを更新する
+    Route::put('/profile/me', [ProfileController::class, 'update'])->name('profile.me');
+    // qr読み取り後
+    Route::get('/profile/{user_id}/preview', [OtherProfileController::class, 'show'])->name('profile.user');
+    // ログアウト
     Route::post('/logout', LogoutController::class)->name('logout');
     
     Route::get('user', function (Request $request) {
